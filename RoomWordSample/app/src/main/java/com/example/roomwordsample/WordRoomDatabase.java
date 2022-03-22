e5689eb93412a8021146f0cf281e202f6028ee54
rafiadiningrat <80728200+rafiadiningrat@users.noreply.github.com>
2022-03-22 20:41:37 +0700

    ViewModel dan LiveData

---
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/WordRoomDatabase.java
@@ -0,0 +1,123 @@
+package com.example.roomwordsample;
+
+import static com.example.roomwordsample.WordRoomDatabase.INSTANCE;
+import static com.example.roomwordsample.WordRoomDatabase.databaseWriteExecutor;
+
+import android.app.Application;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Word.class}, version = 1, exportSchema = false)
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+    public abstract WordDao wordDao();
+
+    public static volatile WordRoomDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static WordRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database")
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            // If you want to keep data through app restarts,
+            // comment out the following block
+            databaseWriteExecutor.execute(() -> {
+                // Populate the database in the background.
+                // If you want to start with more words, just add them.
+                WordDao dao = INSTANCE.wordDao();
+                dao.deleteAll();
+
+                Word word = new Word("Hello");
+                dao.insert(word);
+                word = new Word("World");
+                dao.insert(word);
+            });
+        }
+    };
+
+}
+
+class WordRepository {
+
+    private WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+
+    // Note that in order to unit test the WordRepository, you have to remove the Application
+    // dependency. This adds complexity and much more code, and this sample is not about testing.
+    // See the BasicSample in the android-architecture-components repository at
+    // https://github.com/googlesamples
+    WordRepository(Application application) {
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAlphabetizedWords();
+    }
+
+    // Room executes all queries on a separate thread.
+    // Observed LiveData will notify the observer when the data has changed.
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    // You must call this on a non-UI thread or your app will throw an exception. Room ensures
+    // that you're not doing any long running operations on the main thread, blocking the UI.
+    void insert(Word word) {
+        databaseWriteExecutor.execute(() -> {
+            mWordDao.insert(word);
+        });
+    }
+}
+
+class WordViewHolder extends RecyclerView.ViewHolder {
+    private final TextView wordItemView;
+
+    private WordViewHolder(View itemView) {
+        super(itemView);
+        wordItemView = itemView.findViewById(R.id.textView);
+    }
+
+    public void bind(String text) {
+        wordItemView.setText(text);
+    }
+
+    static WordViewHolder create(ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recyclerview_item, parent, false);
+        return new WordViewHolder(view);
+    }
+}
+
+
+
